67bb88edf90a220b04a65b5122a33e0cd0ac8d7f
Gerardo de León García <gerardodlg.dev@gmail.com>
2026-02-17 14:52:43 -0600

    feat: translating v0 code into reusable components 2

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,11 +1,15 @@
 import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
+import { HeroStats } from "@/heroes/components/HeroStats";
 
 const SearchPage = () => {
   return (
-    <CustomJumbotron
-      title="Search for superheroes"
-      description="Discover, explore, and manage your favorite superheroes and villains"
-    />
+    <>
+      <CustomJumbotron
+        title="Search for superheroes"
+        description="Discover, explore, and manage your favorite superheroes and villains"
+      />
+      <HeroStats />
+    </>
   );
 };
 
